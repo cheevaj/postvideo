828482da3c6abd1a24587ee534b8f9799b6243d6
Chi Vang <Chi@tplus.la>
2024-11-08 14:36:55 +0700

    11 - 8 - 2024

--- pages/loginex.vue
+++ pages/loginex.vue
@@ -1,146 +1,240 @@
 <template>
-    <div class="login-page">
-      <div class="login-card">
-        <h2 class="login-title">Login</h2>
-        <v-text-field
-          label="Email"
-          outlined
-          dense
-          class="login-input"
-        ></v-text-field>
-        <v-text-field
-          label="Password"
-          type="password"
-          outlined
-          dense
-          class="login-input"
-        ></v-text-field>
-        <div class="login-options">
-          <v-checkbox
-            label="Remember Me"
-            class="login-checkbox"
-            dense
-          ></v-checkbox>
-          <a href="#" class="forgot-password">Forget Password</a>
-        </div>
-        <v-btn class="login-button rainbow-button login-card" large>Log in</v-btn>
-        <p class="register-text">
-          Don’t have an account? <a href="#">Register</a>
-        </p>
-      </div>
+  <div>
+    <div>
+      <v-img
+        height="91vh"
+        gradient="to top right, rgba(225,225,0,0.2), rgba(0,0,0,0.2)"
+        class="screen"
+        :src="require('@/static/login_backgroud.png')"
+      >
+        <v-card-text
+          class="justify-center align-center text-center"
+          style="height: 100%"
+        >
+          <v-card
+            class="text-center"
+            max-width="380px"
+            :class="{ shaking: !active }"
+            style="
+              background-color: rgb(255, 224, 0);
+              border: 1.5px solid #ffcc00;
+              border-radius: 20px;
+              margin: 0 auto;
+              top: 10%;
+              box-shadow: 20px 10px 20px rgba(0, 0, 0, 0.5);
+            "
+          >
+            <v-card-actions class="back-BK py-0 pr-0" style="position: relative;">
+              <v-avatar class="my-0 pa-4" size="90">
+                <img src="~/static/Tpluslogo.png" alt="John" />
+              </v-avatar>
+              <div class="pa-0" style="position: relative; display: inline-block; height: 100px; width: 280px; border-top-right-radius: 20px; overflow: hidden;">
+                <img src="~/static/line-yellow-and-black.jpg" alt="John" style="height: 100%; width: 100%; border-top-right-radius: 20px;">
+                <h2 class="text-center custom-font" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); margin: 0; color: #ffff;">
+                  ເຂົ້າສູ່ລະບົບ
+                </h2>
+              </div>
+            </v-card-actions>
+            <v-divider></v-divider>
+            <v-card-actions style="background-color: transparent; color: #000;">
+              <v-form
+                @submit.prevent="checkrequired"
+                style="border-radius: 20px"
+                @keydown.enter="checkrequired"
+              >
+                <v-card-text
+                  style="color: #000; padding-right: 12px; padding-top: 24px"
+                >
+                  <v-row>
+                    <v-col cols="12">
+                      <v-card-actions class="pa-0">
+                        <v-card outlined style="height: 45px; width: 45px; border-radius: 50%; padding-top: 4px; background-color: rgb(242, 242, 242);">
+                          <v-icon size="34">mdi-account-tie</v-icon>
+                        </v-card>
+                        &nbsp;&nbsp;
+                        <v-text-field
+                          solo
+                          class="custom-font shadow-card-login"
+                          v-model="email"
+                          placeholder="ກະລຸນາປ້ອມຊື່"
+                          :rules="[rules.required]"
+                          :label="'ຊື່ຜູ້ໃຊ້ (ສະເພາະເອັດມິນ)'"
+                          :label-color="'#000'"
+                          style="color: #000;"
+                          @keydown.enter="checkrequired"
+                        />
+                      </v-card-actions>
+                    </v-col>
+                    <v-col cols="12" class="py-0">
+                      <br />
+                      <v-card-actions class="pa-0">
+                        <v-card outlined style="height: 45px; width: 45px; border-radius: 50%; padding-top: 4px; background-color: rgb(242, 242, 242);">
+                          <v-icon size="34">{{
+                            !showpassword
+                            ? 'mdi-lock-open'
+                            : 'mdi-lock-open-variant'
+                          }}</v-icon>
+                        </v-card>
+                        &nbsp;&nbsp;
+                        <v-text-field
+                          solo
+                          class="custom-font shadow-card-login"
+                          placeholder="ກະລຸນາປ້ອມລະຫັດຜ່ານ"
+                          v-model="password"
+                          :rules="[rules.required, rules.counter]"
+                          :label="'ລະຫັດຜ່ານ'"
+                          :append-icon="
+                            showpassword ? 'mdi-eye' : 'mdi-eye-off'
+                          "
+                          :type="showpassword ? 'text' : 'password'"
+                          @click:append="showpassword = !showpassword"
+                          counter
+                          maxlength="20"
+                          :label-color="'#000'"
+                          style="color: #000;"
+                          @keydown.enter="checkrequired"
+                        ></v-text-field>
+                      </v-card-actions>
+                    </v-col>
+                  </v-row>
+                </v-card-text>
+                <v-card-text class="text-center mt-12">
+                  <v-btn
+                    outlined
+                    class="custom-font"
+                    style="
+                      background-color: black;
+                      min-width: 150px;
+                      height: 45px;
+                    "
+                    :style="{ color: button ? '#ffff' : '#ffff00' }"
+                    text
+                    @mouseenter="button = false"
+                    @mouseleave="button = true"
+                    @click="checkrequired"
+                  >
+                    <h3>ເຂົ້າສູ່ລະບົບ</h3>
+                  </v-btn>
+                </v-card-text>
+              </v-form>
+            </v-card-actions>
+          </v-card>
+        </v-card-text>
+      </v-img>
     </div>
-  </template>
-  <script>
-  export default {
-    layout: 'default',
-  }
-  </script>
-  
-  <style scoped>
-  .login-page {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    min-height: 90vh;
-    background: url('https://cdn.ghostmarket.io/ext-full/arweave_net_BaK-Ro09sMyPHwNtEZ-AJP61kriOqKbnvdDsKhuCwnw')
-      no-repeat center center;
-    background-size: cover;
-  }
-  .login-card {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 20px;
-    width: 350px;
-    background: rgba(255, 255, 255, 0.1);
-    backdrop-filter: blur(10px);
-    border-radius: 15px;
-    box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
-    text-align: center;
-  }
-  .login-title {
-    font-size: 2em;
-    font-weight: bold;
-    margin-bottom: 20px;
-    color: #fff;
-  }
-  .login-input {
-    width: 100%;
-    margin-bottom: 15px;
-  }
-  .login-options {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    width: 100%;
-    color: #fff;
-  }
-  .login-checkbox {
-    color: #fff;
-  }
-  .forgot-password {
-    color: #fff;
-    text-decoration: none;
-  }
-  .login-button {
-    width: 100%;
-    color: #fff;
-    margin-top: 20px;
-    border-radius: 50px;
-    font-weight: bold;
-  }
-  .rainbow-button {
-    background: linear-gradient(
-      12deg,
-      #ff80bf,
-      #ff66b3,
-      #ff704d,
-      #ff5c33,
-      #ffd633,
-      #ffd11a,
-      #ffff66,
-      #ffff00,
-      #4dff4d,
-      #1aff1a,
-      #6666ff,
-      #4d4dff,
-      #4da6ff,
-      #66b3ff,
-      #66ffff,
-      #4dffff,
-      #ffccff,
-      #ff99ff,
-      #ff80bf,
-      #ff66b3,
-      #ff704d,
-      #ffd633,
-      #ffd11a,
-      #ffff66,
-      #ffff00,
-      #4dff4d,
-      #1aff1a,
-      #66b3ff,
-      #4dffff
-    );
-    color: #fff;
-    background-size: 200% 200%;
-    animation: rainbow-animation 18s linear infinite;
-  }
-  @keyframes rainbow-animation {
-    0% {
-      background-position: 0% 50%;
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      active: true,
+      button: true,
+      showpassword: false,
+      email: '',
+      password: '',
+      rules: {
+        required: (value) => !!value || 'Required.',
+        counter: (value) => value.length <= 20 || 'Max 20 characters',
+      },
     }
-    100% {
-      background-position: 200% 50%;
-    }
+  },
+  mounted() {
+    this.setSheetHeight()
+    window.addEventListener('resize', this.setSheetHeight)
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.setSheetHeight)
+  },
+  methods: {
+    setSheetHeight() {
+      const screen = document.querySelector('.screen')
+      if (screen) {
+        const screenHeight = window.innerHeight + 10
+        screen.style.height = screenHeight + 'px'
+      }
+    },
+    toggleActive() {
+      this.active = false
+      if (!this.active) {
+        setTimeout(() => {
+          this.active = true
+        }, 500)
+      }
+    },
+    async checkrequired() {
+      console.log(this.email, this.password)
+      try {
+        const response = await this.$auth.loginWith('local', {
+          data: {
+            nameoremail: this.email,
+            password: this.password,
+          },
+        })
+        if (response.data) {
+          this.$store.commit('setToken', response.data);
+          this.$router.push('/')
+        } else {
+          console.error('Invalid response data')
+        }
+      } catch (err) {
+        this.errorMessage()
+      }
+    },
+    errorMessage() {
+      this.$Notice.error({
+        title: `<span class="custom-font">${'ບໍ່ພົບຜູ້ໃຊ້ນີ້'}</span>`,
+        desc: `<span class="custom-font">${'ກະລຸນາກວດເບິ່ງຊື່,ອີເມວ ຫຼື ລະຫັດຜ່ານຂອງທ່ານ ແລະ ລອງໃໝ່ອີກ.'}</span>`,
+      })
+      this.toggleActive()
+    },
+    encryptToken(token) {
+      const CryptoJS = require('crypto-js')
+      const secretKey = 'yourSecretKey'
+      const encryptedToken = CryptoJS.AES.encrypt(token, secretKey).toString()
+      return encryptedToken
+    },
+  },
+}
+</script>
+
+<style>
+#screen::-webkit-scrollbar {
+  display: none;
+}
+.back-BK {
+  background-color: #000;
+  color: #ffff00;
+}
+.hovered {
+  background-color: rgba(255, 255, 255, 0.8);
+  border-color: rgb(255, 255, 0);
+}
+.shadow-card-login {
+  height: 45px;
+  border-radius: 16px;
+  background-color: #fff !important;
+  box-shadow: inset 2px 4px 4px 2px rgba(0, 0, 0, 0.5) !important;
+  outline: 0px solid #f2f2f2 !important;
+}
+.shadow-card-login .v-input__control .v-input__slot {
+  background-color: #fff !important;
+  border-radius: 16px;
+  box-shadow: inset 0px 4px 4px 2px rgba(0, 0, 0, 0.5) !important;
+}
+.shaking {
+  animation: shake 0.1s ease-in-out infinite alternate;
+}
+@keyframes shake {
+  0% {
+    transform: translateX(-3px);
   }
-  .register-text {
-    margin-top: 20px;
-    color: #fff;
+
+  100% {
+    transform: translateX(3px);
   }
-  .register-text a {
-    color: #fff;
-    font-weight: bold;
-    text-decoration: none;
-  }
-  </style>
+}
+.custom-font {
+  font-family: 'Noto Sans Lao', sans-serif;
+}
+</style>
